7d5a4e70e4ea89a81a2897f9c23e06539c139e77
Damian Roiz <damianx989@gmail.com>
2022-10-10 12:22:00 -0400

    created components and global styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
+
+const posts = [
+  {
+    title: "The Importance of Stretching",
+    excerpt: "Learn proper stretching techniques",
+  },
+  {
+    title: "How to Prevent Knee Injuries",
+    excerpt: "Learn how to take care of you knee",
+  },
+];
 
 export default function Home() {
   return (
@@ -7,12 +19,19 @@ export default function Home() {
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
-
-
     </div>
-  )
+  );
 }
-
