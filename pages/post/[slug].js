4412dc04ef95a0d209df5970482b020d210c7b7e
Damian Roiz <damianx989@gmail.com>
2022-10-19 21:50:11 -0400

    recent posts and categories side menus

---
+++ pages/post/[slug].js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { getPosts, getPostDetails } from '../../services';
+
+import {PostDetail, Categories, PostWidget, Author, Commnets, CommentsForm } from '../../components'
+
+
+const PostDetails = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PostDetails
--- services/index.js
+++ services/index.js
@@ -40,7 +40,7 @@ export const getPosts = async () => {
 };
 
 export const getRecentPosts = async () => {
-  const query = gql `
+  const query = gql`
     query GetPostDetails() {
       posts(
         orderBy: createdAt_ASC
@@ -54,29 +54,45 @@ export const getRecentPosts = async () => {
         slug
       }
     }
-  `
+  `;
 
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query);
   return result.posts;
-
-}
+};
 
 export const getSimilarPosts = async () => {
-   const query = gql `
-      query GetPostDetails($slug: String!, $categories: [String!]) {
-        posts (
-          where: {slug_not: $slug, AND: { categories_some: { slug_in: $categories }}  }
-          last: 3
-        ) {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
         title
         featuredImage {
           url
         }
         createdAt
         slug
-        }
       }
-   `
-  const result = await request(graphqlAPI, query)
+    }
+  `;
+  const result = await request(graphqlAPI, query);
   return result.posts;
-}
+};
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name 
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+  return result.categories;
+};
