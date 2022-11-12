fefc9197f63aa909ae019ea796c71281a26a8ed7
Damian Roiz <damianx989@gmail.com>
2022-11-12 15:40:19 -0500

    contact info buttons

--- components/SectionInfo.jsx
+++ components/SectionInfo.jsx
@@ -1,44 +1,45 @@
 import React from "react";
 import logo from "../public/atriumLogo.png";
-import Link  from "next/link";
-
+import Link from "next/link";
+import Image from "next/image";
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt, FaFax } from "react-icons/fa";
 
 const SectionInfo = () => {
   return (
     //Add logo img inside the following div
     <div id="inicio" className="sectioninfo">
       <div className="logo-container">
-        <a href="/">
-          <img className="logo" src={logo} alt="logo" />
-        </a>
-    </div>
-
-    <div className="contact-info-container">  
-      <div className="contact-info-card">
-        <div>
-          <i className="card-icon far fa-envelope"></i>
-          <p>admin@atriumphysiotherapy.com</p>
-        </div>
-        <div>
-          <i className="card-icon fas fa-map-marker-alt"></i>
-          <p className="info-address">340 Midpark Way SE - Suite 140<br /> 
-          Calgary, AB,  T2X 1P1</p>
-        </div>
-        <div>
-          <i className="card-icon fas fa-phone"></i>
-          <p>+1(403)-255 4461</p>
-        </div>
-        <div>
-          <i className="card-icon fas fa-fax"></i>
-          <p>+1(403)-259 8776</p>
+        <Image href="/" src={logo} className="logo" />
+      </div>
+      <div className="contact-info-container">
+        <div className="contact-info-buttons">
+          <div className="info-btn">
+            <FaEnvelope className="icon" />
+          </div>
+            <p className="info-btn-data">admin@atriumphysiotherapy.com</p>
+          <div className="info-btn">
+            <FaMapMarkerAlt className="icon" />
+          </div>
+            <p>
+              340 Midpark Way SE - Suite 140
+              <br />
+              Calgary, AB, T2X 1P1
+            </p>
+          <div className="info-btn">
+            <FaPhoneAlt className="icon" />
+          </div>
+            <p>+1(403)-255 4461</p>
+          <div className="info-btn">
+            <FaFax className="icon" />
+          </div>
+            <p>+1(403)-259 8776</p>
         </div>
       </div>
-    </div>
-    <div className="appointment-btn-section">
-          <Link href="/Appointment">
-            <a className="appointment-btn cta-btn">Book Your Appointment</a>
-          </Link>
-    </div>
+      <div className="appointment-btn-section">
+        <Link href="/Appointment">
+          <a className="appointment-btn cta-btn">Book Your Appointment</a>
+        </Link>
+      </div>
     </div>
   );
 };
